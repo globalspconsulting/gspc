f8945fc4ff01955d30d83bf8d1b186d3dc2badee
globalspconsulting <globalspconsulting2019@gmail.com>
2019-03-28 23:14:24 -0500

    header and footer

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import logo from './logo.png';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Global SP Consulting
-          </p>
-          <p>Connecting education to technology </p>
-          <p> Site under construction </p>
-          {/*<a*/}
-            {/*className="App-link"*/}
-            {/*href="https://reactjs.org"*/}
-            {/*target="_blank"*/}
-            {/*rel="noopener noreferrer"*/}
-          {/*>*/}
-            {/*Learn React*/}
-          {/*</a>*/}
-        </header>
-        <footer>
-
-        </footer>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="header">
+                    <div className="container">
+                        <ul id="menu">
+                            <li className="site_name"><a href="index.html">GSPC</a></li>
+                            <li className="link"><a href="#">&raquo; Services</a></li>
+                            <li className="link"><a href="#">&raquo; Projects</a></li>
+                            <li className="link"><a href="#">&raquo; Contact us</a></li>
+                            <li className="link"><a href="#">&raquo; About us</a></li>
+                        </ul>
+                    </div>
+                </header>
+                <article className="content">
+                    <div className="container">
+                        <h1>
+                            Global SP Consulting
+                        </h1>
+                        <p>Connecting education to technology </p>
+                        <p> Site under construction </p>
+                    </div>
+                </article>
+                <footer className="footer">
+                    <p>Posted by: Pooja Upadhyay Email us: <a href="mailto:globalspconsulting2019@gmail.com">
+                        globalspconsulting2019@gmail.com</a>.</p>
+                </footer>
+            </div>
+        );
+    }
 }
 
 export default App;
